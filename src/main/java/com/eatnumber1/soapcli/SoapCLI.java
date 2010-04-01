55d74ee6719b44e23f94425c0313723dbe5364d3
Russell Harmon <russ@eatnumber1.com>
2010-04-01 11:22:26 -0400

    It can now build a fully executable jar.

--- src/main/java/com/eatnumber1/soapcli/SoapCLI.java
+++ src/main/java/com/eatnumber1/soapcli/SoapCLI.java
@@ -1,6 +1,7 @@
 package com.eatnumber1.soapcli;
 
 import com.eatnumber1.soapi.controller.SoapController;
+import com.eatnumber1.soapi.controller.SoapControllerFactory;
 import com.eatnumber1.soapi.controller.SoapLocation;
 import com.eatnumber1.soapi.server.Playlist;
 import com.eatnumber1.soapi.server.SongFile;
@@ -8,14 +9,16 @@ import com.eatnumber1.soapi.server.SongServer;
 import com.eatnumber1.util.MessageBundle;
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,42 +30,84 @@ public class SoapCLI {
     private static Logger log = LoggerFactory.getLogger(SoapCLI.class);
     private static MessageBundle messages = MessageBundle.getMessageBundle(SoapCLI.class);
     private SongServer server;
-    private ControllerThread controller;
+    private SoapController controller;
 
     public static void main( String[] args ) throws Exception {
-        Option localHostOption = new Option("l", "local-host", true, messages.getMessage("com.eatnumber1.soapcli.option.help.local-host"));
-        Option soapHostOption = new Option("n", "hostname", true, messages.getMessage("com.eatnumber1.soapcli.option.help.hostname"));
-        Option southVaderOption = new Option(null, "south-vader", true, messages.getMessage("com.eatnumber1.soapcli.option.help.south-vader"));
+        BasicConfigurator.configure();
+
+        Option verboseOption = new Option("v", "verbose", false, "Enable verbose (INFO) messages");
+        Option debugOption = new Option("d", "debug", false, "Enable debugging messages");
+        Option helpOption = new Option("h", "help", false, "Print this message");
+        Option localHostOption = new Option(null, "local-host", true, messages.getMessage("com.eatnumber1.soapcli.option.help.local-host"));
+        Option localPortOption = new Option(null, "local-port", true, messages.getMessage("com.eatnumber1.soapcli.option.help.local-port"));
+        Option soapHostOption = new Option(null, "soap-host", true, messages.getMessage("com.eatnumber1.soapcli.option.help.soap-host"));
+        Option soapPortOption = new Option(null, "soap-port", true, messages.getMessage("com.eatnumber1.soapcli.option.help.soap-port"));
+        Option southVaderOption = new Option("sv", "south-vader", false, messages.getMessage("com.eatnumber1.soapcli.option.help.south-vader"));
+        Option northVaderOption = new Option("nv", "north-vader", false, messages.getMessage("com.eatnumber1.soapcli.option.help.north-vader"));
+        Option lOption = new Option("l", "the-l", false, messages.getMessage("com.eatnumber1.soapcli.option.help.l"));
+        Option southStairsOption = new Option("ss", "south-stairs", false, messages.getMessage("com.eatnumber1.soapcli.option.help.south-stairs"));
+        Option northStairsOption = new Option("ns", "north-stairs", false, messages.getMessage("com.eatnumber1.soapcli.option.help.north-stairs"));
 
         Options options = new Options();
+        options.addOption(verboseOption);
+        options.addOption(debugOption);
+        options.addOption(helpOption);
         options.addOption(localHostOption);
+        options.addOption(localPortOption);
         options.addOption(soapHostOption);
+        options.addOption(soapPortOption);
         options.addOption(southVaderOption);
+        options.addOption(northVaderOption);
+        options.addOption(lOption);
+        options.addOption(southStairsOption);
+        options.addOption(northStairsOption);
 
         CommandLineParser parser = new GnuParser();
         CommandLine cmd = parser.parse(options, args, false);
 
         SoapCLI cli;
-        SoapController controller = new SoapController();
+        SoapControllerFactory controllerFactory = new SoapControllerFactory();
 
-        URI localUri = controller.getLocalUri();
-        if( cmd.hasOption(localHostOption.getOpt()) ) {
-            controller.setLocalUri(new URI(localUri.getScheme(), localUri.getUserInfo(), cmd.getOptionValue(localHostOption.getOpt()), localUri.getPort(), localUri.getPath(), localUri.getQuery(), localUri.getFragment()));
+        org.apache.log4j.Logger rootLogger = org.apache.log4j.Logger.getLogger("com.eatnumber1");
+        if( cmd.hasOption(debugOption.getLongOpt()) ) {
+            rootLogger.setLevel(Level.DEBUG);
+        } else {
+            org.apache.log4j.Logger.getRootLogger().setLevel(Level.WARN);
+            if( cmd.hasOption(verboseOption.getLongOpt()) ) {
+                rootLogger.setLevel(Level.INFO);
+            } else {
+                rootLogger.setLevel(Level.WARN);
+            }
         }
-
-        URI soapUri = controller.getSoapUri();
-        if( cmd.hasOption(soapHostOption.getOpt()) ) {
-            controller.setSoapUri(new URI(soapUri.getScheme(), soapUri.getUserInfo(), cmd.getOptionValue(soapHostOption.getOpt()), soapUri.getPort(), soapUri.getPath(), soapUri.getQuery(), soapUri.getFragment()));
+        if( cmd.hasOption(helpOption.getLongOpt()) ) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("soapcli [options] (location option) songs...", options);
+            System.exit(0);
         }
-
+        if( cmd.hasOption(localHostOption.getLongOpt()) )
+            controllerFactory.setLocalHost(cmd.getOptionValue(localHostOption.getLongOpt()));
+        if( cmd.hasOption(localPortOption.getLongOpt()) )
+            controllerFactory.setLocalPort(Integer.valueOf(cmd.getOptionValue(localPortOption.getLongOpt())));
+        if( cmd.hasOption(soapHostOption.getLongOpt()) )
+            controllerFactory.setSoapHost(cmd.getOptionValue(soapHostOption.getLongOpt()));
+        if( cmd.hasOption(soapPortOption.getLongOpt()) )
+            controllerFactory.setSoapPort(Integer.valueOf(cmd.getOptionValue(soapPortOption.getLongOpt())));
         if( cmd.hasOption(southVaderOption.getLongOpt()) ) {
-            controller.setLocation(SoapLocation.SOUTH_VADER);
+            controllerFactory.setLocation(SoapLocation.SOUTH_VADER);
+        } else if( cmd.hasOption(northVaderOption.getLongOpt()) ) {
+            controllerFactory.setLocation(SoapLocation.NORTH_VADER);
+        } else if( cmd.hasOption(lOption.getLongOpt()) ) {
+            controllerFactory.setLocation(SoapLocation.THE_L);
+        } else if( cmd.hasOption(southStairsOption.getLongOpt()) ) {
+            controllerFactory.setLocation(SoapLocation.SOUTH_STAIRS);
+        } else if( cmd.hasOption(northStairsOption.getLongOpt()) ) {
+            controllerFactory.setLocation(SoapLocation.NORTH_STAIRS);
         } else {
             log.error(messages.getMessage("com.eatnumber1.soapcli.error.no-location"));
             System.exit(1);
         }
 
-        cli = new SoapCLI(controller, cmd.getArgs());
+        cli = new SoapCLI(controllerFactory.produce(), cmd.getArgs());
         cli.run();
     }
 
@@ -71,26 +116,26 @@ public class SoapCLI {
         for( String s : songs ) songList.add(new SongFile(new File(s)));
         Playlist playlist = new Playlist(songList);
         server = new SongServer(playlist);
-        this.controller = new ControllerThread(controller);
+        this.controller = controller;
     }
 
     public void run() throws Exception {
-        new Thread(controller).run();
-        server.start();
+        new Thread(new ServerThread(server), "Server Thread").start();
+        server.waitForStart();
+        controller.play();
     }
 
-    public class ControllerThread implements Runnable {
-        private SoapController controller;
+    public class ServerThread implements Runnable {
+        private SongServer server;
 
-        public ControllerThread( SoapController controller ) throws IOException {
-            this.controller = controller;
+        public ServerThread( SongServer server ) {
+            this.server = server;
         }
 
         @Override
         public void run() {
             try {
-                server.join();
-                controller.play();
+                server.start();
             } catch( Exception e ) {
                 if( e instanceof RuntimeException ) throw (RuntimeException) e;
                 throw new RuntimeException(e);
