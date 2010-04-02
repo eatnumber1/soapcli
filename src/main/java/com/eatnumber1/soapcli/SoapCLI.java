56771dce5b8f78154a2e4df61978cce20ea6fe41
Russell Harmon <russ@eatnumber1.com>
2010-04-02 14:13:31 -0400

    Properly exit on song finish

--- src/main/java/com/eatnumber1/soapcli/SoapCLI.java
+++ src/main/java/com/eatnumber1/soapcli/SoapCLI.java
@@ -9,21 +9,15 @@ import com.eatnumber1.soapi.server.SongServer;
 import com.eatnumber1.util.MessageBundle;
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.CyclicBarrier;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.slf4j.Logger;
@@ -39,12 +33,12 @@ public class SoapCLI {
     private SongServer server;
     private SoapController controller;
 
-    public static void main( String[] args ) throws ParseException, URISyntaxException, IOException {
+    public static void main( String[] args ) throws Exception {
         BasicConfigurator.configure();
 
-        Option verboseOption = new Option("v", "verbose", false, "Enable verbose (INFO) messages");
-        Option debugOption = new Option("d", "debug", false, "Enable debugging messages");
-        Option helpOption = new Option("h", "help", false, "Print this message");
+        Option verboseOption = new Option("v", "verbose", false, messages.getMessage("com.eatnumber1.soapcli.option.help.verbose"));
+        Option debugOption = new Option("d", "debug", false, messages.getMessage("com.eatnumber1.soapcli.option.help.debug"));
+        Option helpOption = new Option("h", "help", false, messages.getMessage("com.eatnumber1.soapcli.option.help.help"));
         Option localHostOption = new Option(null, "local-host", true, messages.getMessage("com.eatnumber1.soapcli.option.help.local-host"));
         Option localPortOption = new Option(null, "local-port", true, messages.getMessage("com.eatnumber1.soapcli.option.help.local-port"));
         Option soapHostOption = new Option(null, "soap-host", true, messages.getMessage("com.eatnumber1.soapcli.option.help.soap-host"));
@@ -88,7 +82,7 @@ public class SoapCLI {
         }
         if( cmd.hasOption(helpOption.getLongOpt()) ) {
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("soapcli [options] (location option) songs...", options);
+            formatter.printHelp(messages.getMessage("com.eatnumber1.soapcli.usage"), options);
             System.exit(0);
         }
         if( cmd.hasOption(localHostOption.getLongOpt()) )
@@ -126,39 +120,11 @@ public class SoapCLI {
         this.controller = controller;
     }
 
-    public void run() {
-        ExecutorService threadPool = Executors.newCachedThreadPool(new ThreadFactory() {
-            @Override
-            public Thread newThread( Runnable r ) {
-                return new Thread(r, "SoapCLI Thread");
-            }
-        });
-        List<Future<Void>> futures = new ArrayList<Future<Void>>(2);
-        try {
-            futures.add(threadPool.submit(new ControllerCallable()));
-            futures.add(threadPool.submit(new ServerCallable()));
-            for( Future<Void> f : futures ) f.get();
-        } catch( Exception e ) {
-            threadPool.shutdownNow();
-        } finally {
-            threadPool.shutdown();
-        }
-    }
-
-    public class ServerCallable implements Callable<Void> {
-        @Override
-        public Void call() throws Exception {
-            server.start();
-            return null;
-        }
-    }
-
-    public class ControllerCallable implements Callable<Void> {
-        @Override
-        public Void call() throws Exception {
-            server.waitForStart();
-            controller.play();
-            return null;
-        }
+    public void run() throws Exception {
+        CyclicBarrier barrier = server.start();
+        //controller.play();
+        barrier.await();
+        Thread.sleep(1000);
+        server.stop();
     }
 }
